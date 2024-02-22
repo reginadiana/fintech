caf9bc47268ce3cfe9ebf861015829fdf1c9beff
Diana Regina <dianaregina22@outlook.com.br>
2024-02-22 09:58:40 -0300

    config: add resolve path to vite, feat: add header, summary and sidebar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import './Style.css';
+import Header from 'src/components/Header';
 
-function App() {
-  return <div>Fintech</div>;
+import Sidebar from 'src/components/Sidebar';
+import Summary from 'src/pages/Summary';
+
+import './style.css';
+
+/**
+ * TODO: 22/02/24
+ * - Estou na aula 0402 Páginas e Hooks
+ * - Parei em 01:21
+ */
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Summary />
+        <Sidebar />
+      </main>
+    </>
+  );
 }
-
-export default App;
