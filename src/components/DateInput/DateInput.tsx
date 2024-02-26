92dada38807a4bfdac71315db3c5d71575dc1816
Diana Regina <dianaregina22@outlook.com.br>
2024-02-26 09:19:09 -0300

    layout: css properties

--- src/components/DateInput/DateInput.tsx
+++ src/components/DateInput/DateInput.tsx
@@ -1,12 +1,35 @@
-import { ComponentProps } from 'react';
+import { CSSProperties, ComponentProps } from 'react';
+
+const generalStyle: CSSProperties = {
+  fontSize: '1rem',
+  color: 'var(--color-2)',
+  padding: 'var(--gap-s) .75rem',
+  backgroundColor: 'var(--color-4)',
+  borderRadius: 'var(--gap)',
+};
+
+const labelStyle: CSSProperties = {
+  display: 'block',
+  marginBottom: 'var(--gap-s)',
+  fontWeight: '600',
+  ...generalStyle,
+};
+
+const inputStyle: CSSProperties = {
+  border: 'none',
+  fontFamily: 'monospace',
+  ...generalStyle,
+};
 
 type Props = ComponentProps<'input'> & { label: string };
 
 export default function DateInput({ label, ...rest }: Props) {
   return (
     <div>
-      <label htmlFor={label}>{label}</label>
-      <input id={label} name={label} type="date" {...rest} />
+      <label style={labelStyle} htmlFor={label}>
+        {label}
+      </label>
+      <input style={inputStyle} id={label} name={label} type="date" {...rest} />
     </div>
   );
 }
